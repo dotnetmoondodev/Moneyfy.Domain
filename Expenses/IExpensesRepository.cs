bcec3bd4fcea58c1ad079b430585794031010ade
Hector Gomez <moon.do.dev@gmail.com>
2025-07-01 22:44:09 -0500

    changes required to implement mongodb persistence

--- Expenses/IExpensesRepository.cs
+++ Expenses/IExpensesRepository.cs
@@ -1,8 +1,12 @@
+using System.Linq.Expressions;
+
 namespace Domain.Expenses;
 
 public interface IExpensesRepository
 {
     Task<Expense?> GetByIdAsync( Guid id, CancellationToken cancellationToken );
+    Task<Expense?> GetByIdAsync( Expression<Func<Expense, bool>> filter, CancellationToken cancellationToken );
+    Task<IReadOnlyCollection<Expense>> GetAllAsync( Expression<Func<Expense, bool>>? filter, CancellationToken cancellationToken );
     Task AddAsync( Expense expense, CancellationToken cancellationToken );
     Task UpdateAsync( Expense expense, CancellationToken cancellationToken );
     Task DeleteAsync( Expense expense, CancellationToken cancellationToken );
--- Payments/IPaymentsRepository.cs
+++ Payments/IPaymentsRepository.cs
@@ -1,8 +1,12 @@
+using System.Linq.Expressions;
+
 namespace Domain.Payments;
 
 public interface IPaymentsRepository
 {
     Task<Payment?> GetByIdAsync( Guid id, CancellationToken cancellationToken );
+    Task<Payment?> GetByIdAsync( Expression<Func<Payment, bool>> filter, CancellationToken cancellationToken );
+    Task<IReadOnlyCollection<Payment>> GetAllAsync( Expression<Func<Payment, bool>>? filter, CancellationToken cancellationToken );
     Task AddAsync( Payment payment, CancellationToken cancellationToken );
     Task UpdateAsync( Payment payment, CancellationToken cancellationToken );
     Task DeleteAsync( Payment payment, CancellationToken cancellationToken );
