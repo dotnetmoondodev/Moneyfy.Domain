1f56d1a644a7625a515bf160036bde822db94b67
Hector Gomez <moon.do.dev@gmail.com>
2025-07-30 21:44:59 -0500

    changed mongo database to sql-server

--- IRepository.cs
+++ IRepository.cs
@@ -1,12 +1,9 @@
-using System.Linq.Expressions;
-
 namespace Domain;
 
 public interface IRepository<T>
 {
+    Task<IReadOnlyCollection<T>> GetAllAsync( CancellationToken cancellationToken );
     Task<T?> GetByIdAsync( Guid id, CancellationToken cancellationToken );
-    Task<T?> GetByIdAsync( Expression<Func<T, bool>> filter, CancellationToken cancellationToken );
-    Task<IReadOnlyCollection<T>> GetAllAsync( Expression<Func<T, bool>>? filter, CancellationToken cancellationToken );
     Task AddAsync( T entity, CancellationToken cancellationToken );
     Task UpdateAsync( T entity, CancellationToken cancellationToken );
     Task DeleteAsync( T entity, CancellationToken cancellationToken );
